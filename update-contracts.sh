cf4fbba3052c95c1f1a415b681fbded8a4bf7057
zemse <sohamzemse143@gmail.com>
2021-01-02 13:11:34 +0530

    Add abis

--- update-contracts.sh
+++ update-contracts.sh
@@ -1,4 +1,8 @@
+rm -r dollar
 git clone https://github.com/emptysetsquad/dollar.git
 rm -r contracts
 mkdir contracts
 cp -r dollar/protocol/contracts/. contracts/.
+rm -r abis
+mkdir abis
+cp -r dollar/protocol/abi/. abis/.
